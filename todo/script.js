0792a50c6bcce081016dcd097b9802981d8b1c2c
Rokas <rokamanauskas@gmail.com>
2022-05-23 21:02:40 +0300

    Add POST and entry rendering functionality

--- todo/script.js
+++ todo/script.js
@@ -1,26 +1,27 @@
+const outputContainer = document.getElementById('entryContainer')
 const addButton = document.getElementById('addButton')
 
-addButton.addEventListener("click", () => {
-    addButton.style.display = "none"
+addButton.addEventListener('click', () => {
+    addButton.style.display = 'none'
     newEntryInput.style.display = 'inline-block'
 })
 
-const cancelButton = document.getElementById("cancelEntry")
-cancelButton.addEventListener("click", () => {
-    addButton.style.display = "block"
+const cancelButton = document.getElementById('cancelEntry')
+cancelButton.addEventListener('click', () => {
+    addButton.style.display = 'block'
     newEntryInput.style.display = 'none'
     
 })
 
-const saveButton = document.getElementById("saveEntry")
-saveButton.addEventListener("click", () => saveEntry())
+const saveButton = document.getElementById('saveEntry')
+saveButton.addEventListener('click', () => saveEntry())
 
 function saveEntry() {
     let entryType = document.getElementById('newEntryType').value;
     let entryContent = document.getElementById('newEntryContent').value;
     let entryEndDate = document.getElementById('newEntryEndDate').value;
     // placeholder value (maybe change to number?)
-    let entryUserId = "2"
+    let entryUserId = '2'
 
     fetch('https://testapi.io/api/SurkusAPI/resource/ToDoList', {
         method: 'POST',
@@ -41,9 +42,57 @@ function saveEntry() {
     })
     .then((result) => {
         console.log(result);
-        entryType = "";
-        entryContent = "";
+        entryType = '';
+        entryContent = '';
         entryEndDate = null;
         // render all tasks including the new one here
     })
 }
+
+function getEntries() {
+    outputContainer.innerHTML = ''
+    fetch('https://testapi.io/api/SurkusAPI/resource/ToDoList')
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(result => render(result.data)); 
+}
+
+function render(entries) {
+    entries.forEach(entry => {
+        const div = document.createElement('div');
+        div.id = 'containerItem'
+
+        const type = document.createElement('p');
+        type.id = 'entryType';
+        type.textContent = entry.type;
+
+        const content = document.createElement('p');
+        content.id = 'entryContent';
+        content.textContent = entry.content;
+
+        const endDate = document.createElement('p');
+
+        endDate.id = 'entryEndDate';
+        endDate.textContent = entry.endDate;
+
+        const delButton = document.createElement('button');
+        delButton.textContent = 'DELETE';
+        delButton.addEventListener('click', (event) => {
+            const elementId = event.target.parentElement.id;
+            deleteEntry(elementId);
+        })
+
+        div.append(type, content, endDate, delButton);
+        div.setAttribute('id', entry.id);
+        outputContainer.append(div);
+    })
+}
+
+function deleteEntry() {
+    
+}
+
+getEntries()
