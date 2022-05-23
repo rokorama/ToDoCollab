6e98b91a5337c025df309fa4920781ea98239132
SurkusGH <v.surkus@me.com>
2022-05-23 17:51:26 +0300

    Added error message for signUp name, for testing purposes

--- login/script.js
+++ login/script.js
@@ -1,3 +1,23 @@
+function setFormMessage(formElement, type, message){
+    const messageElement = formElement.querySelector(".form_message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove('form_message-success', 'form_message-error')
+    messageElement.classList.add(`form_message-${type}`)
+}
+
+//setFormMessage(loginForm, "success", "Prisijungėte!")
+
+function setInputError(inputElement, message){
+    inputElement.classList.add("form_input-error");
+    inputElement.parentElement.querySelector(".form_input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement){
+    inputElement.classList.remove("form_input-error")
+    inputElement.parentElement.querySelector(".form_input-error-message").textContent= "";
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector('#login')
     const createAccountForm = document.querySelector('#createAccount')
@@ -13,4 +33,23 @@ document.addEventListener("DOMContentLoaded", () => {
         loginForm.classList.remove("form-hidden");
         createAccountForm.classList.add("form-hidden");
     });
+
+    loginForm.addEventListener("submit", event => {
+        event.preventDefault();
+
+        //fetch login
+
+        setFormMessage(loginForm, "error", "Neteisingas vartotojas/slaptažodis!")
+    });
+
+    document.querySelectorAll(".form_input").forEach(inputElement => {
+        inputElement.addEventListener("blur", event => {
+            if (event.target.id === "signUpUserName" && event.target.value.length > 0 && event.target.value.length < 5) {
+                setInputError(inputElement, "Vartotojas turi turėti bent 5 simbolius!");
+            }
+        });
+        inputElement.addEventListener("input", event => {
+            clearInputError(inputElement)
+        })
+    })
 });
