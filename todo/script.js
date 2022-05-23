687c84fd4da471636c6e20de0b8f5f9802935224
Rokas <rokamanauskas@gmail.com>
2022-05-23 20:14:35 +0300

    Add POST functionality

--- todo/script.js
+++ todo/script.js
@@ -13,4 +13,37 @@ cancelButton.addEventListener("click", () => {
 })
 
 const saveButton = document.getElementById("saveEntry")
-saveButton.addEventListener("click", () => void 0)
+saveButton.addEventListener("click", () => saveEntry())
+
+function saveEntry() {
+    let entryType = document.getElementById('newEntryType').value;
+    let entryContent = document.getElementById('newEntryContent').value;
+    let entryEndDate = document.getElementById('newEntryEndDate').value;
+    // placeholder value (maybe change to number?)
+    let entryUserId = "2"
+
+    fetch('https://testapi.io/api/SurkusAPI/resource/ToDoList', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            type: entryType,
+            content: entryContent,
+            endDate: entryEndDate,
+            userName: entryUserId
+        })
+    })
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        }
+    })
+    .then((result) => {
+        console.log(result);
+        entryType = "";
+        entryContent = "";
+        entryEndDate = null;
+        // render all tasks including the new one here
+    })
+}
