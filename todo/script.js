003633c75e85ca36121a0c7bfeaf49e304ea588e
Rokas <rokamanauskas@gmail.com>
2022-05-24 19:41:55 +0300

    Add rudimentary PUT functionality for editing entries

--- todo/script.js
+++ todo/script.js
@@ -44,6 +44,8 @@ function saveEntry() {
         console.log(result);
         entryType = '';
         entryContent = '';
+        // add check for past dates
+        // add time functionality?
         entryEndDate = null;
         getEntries()
     })
@@ -65,6 +67,7 @@ function render(entries) {
         const div = document.createElement('div');
         div.className = 'entryItem'
 
+        // rename 'type' to something else or else it causes nightmares below
         const type = document.createElement('p');
         type.id = 'entryType';
         type.textContent = entry.type;
@@ -78,17 +81,71 @@ function render(entries) {
         endDate.id = 'entryEndDate';
         endDate.textContent = entry.endDate;
 
+        const editButton = document.createElement('button');
+        editButton.textContent = 'EDIT';
+        editButton.addEventListener('click', (event) => {
+            // create input form in div
+            // type.outerHTML = (`<input type="text" id="editedType" value=${entry.type}>`);
+            type.outerHTML = (`<input type="text" id="editedType">`);
+            editedType.value = entry.type
+            // content.outerHTML = (`<input type="text" id="editedContent" value=${entry.content}>`);
+            content.outerHTML = (`<input type="text" id="editedContent">`);
+            editedContent.value = entry.content
+            endDate.outerHTML = (`<input type="date" id="editedEndDate" value=${entry.endDate}>`);
+            
+            // entryType.setAttribute('contentEditable', 'true')
+            // entryContent.setAttribute('contentEditable', 'true')
+            // entryEndDate.setAttribute('contentEditable', 'true')
+            
+            const elementId = event.target.parentElement.id;
+
+            console.log(type.textContent, content.textContent, endDate.textContent)
+
+            
+            const confirmEditButton = document.createElement('button');
+            confirmEditButton.textContent = 'SAVE CHANGES';
+            div.append(confirmEditButton);
+            
+            
+            confirmEditButton.addEventListener('click', () => {
+                let updatedType = document.getElementById('editedType').value
+                let updatedContent = document.getElementById('editedContent').value
+                let updatedEndDate = document.getElementById('editedEndDate').value
+                console.log(updatedType, updatedContent, updatedEndDate)
+                editEntry(elementId, updatedType, updatedContent, updatedEndDate)
+            })
+        })
+
         const delButton = document.createElement('button');
         delButton.textContent = 'DELETE';
         delButton.addEventListener('click', (event) => {
             const elementId = event.target.parentElement.id;
             deleteEntry(elementId);
         })
-
-        div.append(type, content, endDate, delButton);
+        
+        div.append(type, content, endDate, editButton, delButton);
         div.setAttribute('id', entry.id);
         outputContainer.append(div);
     })
+}
+
+async function editEntry(entryId, newType, newContent, newEndDate) {
+    const edit = await fetch(`https://testapi.io/api/SurkusAPI/resource/ToDoList/${entryId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            type: newType,
+            content: newContent,
+            endDate: newEndDate,
+        })
+    })
+    
+    if (edit) {
+        getEntries()
+    }
+
 }
 
 async function deleteEntry(entryId) {
@@ -100,5 +157,7 @@ async function deleteEntry(entryId) {
         getEntries()
     }
 }
+
+
 
 getEntries()
